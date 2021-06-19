8f1e1184a984d4be6ca0a3081dfde9a99c5ad5fe
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-06-18 23:41:34 -0400

    added command to get git diff

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@ import { getInput} from "@actions/core";
 import { exec } from "@actions/exec";
 import { context } from "@actions/github";
 
-
-
 const run = async (): Promise<void> => {
     const lineLimit = getInput("lineLimit", { required: true });
-    
+    const { sha } = context;
+    let output = "";
+    let errorOutput = "";
+    const options = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                output += data.toString()
+            },
+            stderr: (data: Buffer) => {
+                errorOutput += data.toString()
+            }
+        }
+    }
+    await exec(`git diff --shortstat origin/main ${sha} | awk '{print $4}'`)
 }
