4f471e45ea2aebb46ad474067e006b8682f4773d
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-06-18 23:58:43 -0400

    pivoting to make both added and removed limitable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { getInput} from "@actions/core";
+import { getInput, setFailed, error } from "@actions/core";
 import { exec } from "@actions/exec";
 import { context } from "@actions/github";
 
 const run = async (): Promise<void> => {
-    const lineLimit = getInput("lineLimit", { required: true });
+    const addLimit = getInput("addLimit", { required: true });
+    const removeLimit = getInput("removeLimit", { required: true });
     const { sha } = context;
     let output = "";
     let errorOutput = "";
@@ -17,5 +18,16 @@ const run = async (): Promise<void> => {
             }
         }
     }
-    await exec(`git diff --shortstat origin/main ${sha} | awk '{print $4}'`)
+    await exec(`git diff --shortstat origin/main ${sha} | awk '{print $4, $6}'`);
+    if(errorOutput) {
+        setFailed(`Error while determining lines changed --- ${errorOutput}`);
+        return;
+    }
+    if(!output) {
+        setFailed(`Unable to get git diff`);
+        return;
+    }
+    const diffs = output.split(" ");
+    const added = diffs[0];
+    const removed = diffs[1];
 }
